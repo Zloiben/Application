6d8b189f373710117dc7f2b160f9ce2a7dfd19a7
Zloiben <daniil2019.milkork@yandex.ru>
2021-09-27 02:42:22 +0800

    .

--- main.py
+++ main.py
@@ -6,17 +6,23 @@ from config import *
 # TODO: Улутшить интерфейс программы
 
 
+def film_window():
+    films.show()
+    ex.close()
+
+
 class Main(QMainWindow):
 
     def __init__(self):
         super().__init__()
 
         #
-        global serial, films
+
+        global serial, films, books_and_comics
 
         films = Films()
         serial = Serials()
-        self.books_and_comics = BooksComics()
+        books_and_comics = BooksComics()
 
         #
 
@@ -24,22 +30,18 @@ class Main(QMainWindow):
 
         #
 
-        self.btn_films.clicked.connect(self.film_window)
+        self.btn_films.clicked.connect(film_window)
         self.btn_serials.clicked.connect(self.serial_window)
         self.btn_books_and_comics.clicked.connect(self.books_and_comics_window)
 
         self.btn_exit_main.clicked.connect(self.exit)
-
-    def film_window(self):
-        films.show()
-        ex.close()
 
     def serial_window(self):
         serial.show()
         ex.close()
 
     def books_and_comics_window(self):
-        self.books_and_comics.show()
+        books_and_comics.show()
         ex.close()
 
     def exit(self):
@@ -73,22 +75,24 @@ class Films(QMainWindow):
         self.search_btn.clicked.connect(self.search_criteria)
     # ----------------------------------------------<Базовый вывод>-----------------------------------------------------
         count = 1
+        self.table_films.appendPlainText('№. film, [rating], (release), style, |description|\n')
         for value in sql.execute("SELECT * FROM data ORDER BY rating DESC"):
             self.table_films.appendPlainText(f'{count}. '
                                              f'{value[0]}, '
                                              f'[{value[1]}], '
                                              f'({value[2]}), '
                                              f'{value[3]}, '
-                                             f'{value[4]}')
+                                             f'\nОписание: {value[4]}\n')
             count += 1
 
     # ----------------------------------------------<Основные Критерии>-------------------------------------------------
 
-    # №. film, [rating], (release), style, description
+    # №. film, [rating], (release), style, |description|
     # 1. Дюна, [8.1], (2021-09-09), Фантастика, Описание
 
     def output_of_films_by_rating(self):
         self.table_films.clear()
+        self.table_films.appendPlainText('№. film, [rating], (release), style, |description|\n')
         count = 1
         if len(self.data_criteria) > 0:
             for value in sql.execute(f"""
@@ -98,7 +102,7 @@ class Films(QMainWindow):
                                                  f'[{value[1]}], '
                                                  f'({value[2]}), '
                                                  f'{value[3]}, '
-                                                 f'{value[4]}')
+                                                 f'\nОписание: |{value[4]}|\n')
                 count += 1
         else:
             for value in sql.execute("SELECT * FROM data ORDER BY rating DESC"):
@@ -107,11 +111,12 @@ class Films(QMainWindow):
                                                  f'[{value[1]}], '
                                                  f'({value[2]}), '
                                                  f'{value[3]}, '
-                                                 f'{value[4]}')
+                                                 f'\nОписание: |{value[4]}|\n')
                 count += 1
 
     def output_of_films_by_date(self):
         self.table_films.clear()
+        self.table_films.appendPlainText('№. film, [rating], (release), style, |description|\n')
         count = 1
         if len(self.data_criteria) > 0:
             for value in sql.execute(f"""
@@ -121,7 +126,7 @@ class Films(QMainWindow):
                                                  f'[{value[1]}], '
                                                  f'({value[2]}), '
                                                  f'{value[3]}, '
-                                                 f'{value[4]}')
+                                                 f'\nОписание: |{value[4]}|\n')
                 count += 1
         else:
             for value in sql.execute("SELECT * FROM data ORDER BY release DESC"):
@@ -130,11 +135,12 @@ class Films(QMainWindow):
                                                  f'[{value[1]}], '
                                                  f'({value[2]}), '
                                                  f'{value[3]}, '
-                                                 f'{value[4]}')
+                                                 f'\nОписание: |{value[4]}|\n')
                 count += 1
 
     def output_of_films_by_name(self):
         self.table_films.clear()
+        self.table_films.appendPlainText('№. film, [rating], (release), style, |description|\n')
         count = 1
         if len(self.data_criteria) > 0:
             for value in sql.execute(f"""
@@ -144,7 +150,7 @@ class Films(QMainWindow):
                                                  f'[{value[1]}], '
                                                  f'({value[2]}), '
                                                  f'{value[3]}, '
-                                                 f'{value[4]}')
+                                                 f'\nОписание: |{value[4]}|\n')
                 count += 1
         else:
             for value in sql.execute("SELECT * FROM data ORDER BY film ASC"):
@@ -153,7 +159,7 @@ class Films(QMainWindow):
                                                  f'[{value[1]}], '
                                                  f'({value[2]}), '
                                                  f'{value[3]}, '
-                                                 f'{value[4]}')
+                                                 f'\nОписание: |{value[4]}|\n')
                 count += 1
 
     # --------------------------------------------Сортировка Критерий---------------------------------------------------
@@ -174,6 +180,7 @@ class Films(QMainWindow):
 
     def search_criteria(self):
         self.table_films.clear()
+        self.table_films.appendPlainText('№. film, [rating], (release), style, |description|\n')
         count = 1
         for value in sql.execute(f"SELECT * FROM data WHERE style in {self.sort()} ORDER BY rating DESC"):
             self.table_films.appendPlainText(f'{count}. '
@@ -181,7 +188,7 @@ class Films(QMainWindow):
                                              f'[{value[1]}], '
                                              f'({value[2]}), '
                                              f'{value[3]}, '
-                                             f'{value[4]}')
+                                             f'\nОписание: |{value[4]}|\n')
             count += 1
 
     #  -----------------------------------------------------------------------------------------------------------------
@@ -196,8 +203,6 @@ class Serials(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('serials.ui', self)
-
-        # TODO: Добавить сортировку по критериям пользователя
 
         self.data_criteria_serials = set()
 
@@ -223,6 +228,7 @@ class Serials(QMainWindow):
         # 1. Воскресший Эртугрул, [8.2], 2014-12-10, (боевик), 5, |Описание|
 
         count = 1
+        self.table_serials.appendPlainText("№. serial, [rating], (release), style, seasons, |description|\n")
         for value in sql.execute("SELECT * FROM data_serials ORDER BY release DESC"):
             self.table_serials.appendPlainText(f'{count}. '
                                                f'{value[0]}, '
@@ -230,11 +236,12 @@ class Serials(QMainWindow):
                                                f'{value[2]}, '
                                                f'({value[3]}), '
                                                f'{value[4]}, '
-                                               f'|{value[5]}|')
+                                               f'\nОписание: |{value[5]}|\n')
             count += 1
 
     def output_of_serials_by_rating(self):
         self.table_serials.clear()
+        self.table_serials.appendPlainText("№. serial, [rating], (release), style, seasons, |description|\n")
         if len(self.data_criteria_serials) > 0:
             count = 1
             for value in sql.execute(f"""
@@ -245,7 +252,7 @@ class Serials(QMainWindow):
                                                    f'{value[2]}, '
                                                    f'({value[3]}), '
                                                    f'{value[4]}, '
-                                                   f'|{value[5]}|')
+                                                   f'\nОписание: |{value[5]}|\n')
                 count += 1
         else:
             count = 1
@@ -256,11 +263,12 @@ class Serials(QMainWindow):
                                                    f'{value[2]}, '
                                                    f'({value[3]}), '
                                                    f'{value[4]}, '
-                                                   f'|{value[5]}|')
+                                                   f'\nОписание: |{value[5]}|\n')
                 count += 1
 
     def output_of_serials_by_date(self):
         self.table_serials.clear()
+        self.table_serials.appendPlainText("№. serial, [rating], (release), style, seasons, |description|\n")
         if len(self.data_criteria_serials) > 0:
             count = 1
             for value in sql.execute(f"""
@@ -271,7 +279,7 @@ class Serials(QMainWindow):
                                                    f'{value[2]}, '
                                                    f'({value[3]}), '
                                                    f'{value[4]}, '
-                                                   f'|{value[5]}|')
+                                                   f'\nОписание: |{value[5]}|\n')
                 count += 1
         else:
             count = 1
@@ -282,11 +290,12 @@ class Serials(QMainWindow):
                                                    f'{value[2]}, '
                                                    f'({value[3]}), '
                                                    f'{value[4]}, '
-                                                   f'|{value[5]}|')
+                                                   f'\nОписание: |{value[5]}|\n')
                 count += 1
 
     def output_of_serials_by_name(self):
         self.table_serials.clear()
+        self.table_serials.appendPlainText("№. serial, [rating], (release), style, seasons, |description|\n")
         if len(self.data_criteria_serials) > 0:
             count = 1
             for value in sql.execute(f"""
@@ -297,7 +306,7 @@ class Serials(QMainWindow):
                                                    f'{value[2]}, '
                                                    f'({value[3]}), '
                                                    f'{value[4]}, '
-                                                   f'|{value[5]}|')
+                                                   f'\nОписание: |{value[5]}|\n')
                 count += 1
         else:
             count = 1
@@ -309,7 +318,7 @@ class Serials(QMainWindow):
                                                    f'{value[2]}, '
                                                    f'({value[3]}), '
                                                    f'{value[4]}, '
-                                                   f'|{value[5]}|')
+                                                   f'\nОписание: |{value[5]}|\n')
                 count += 1
 
     # --------------------------------------------Сортировка Критерий---------------------------------------------------
@@ -330,6 +339,7 @@ class Serials(QMainWindow):
 
     def search_criteria(self):
         self.table_serials.clear()
+        self.table_serials.appendPlainText("№. serial, [rating], (release), style, seasons, |description|\n")
         count = 1
         for value in sql.execute(f"SELECT * FROM data_serials WHERE style in {self.sort()} ORDER BY rating DESC"):
             self.table_serials.appendPlainText(f'{count}. '
@@ -338,7 +348,7 @@ class Serials(QMainWindow):
                                                f'({value[2]}), '
                                                f'{value[3]}, '
                                                f'{value[4]}, '
-                                               f'|{value[5]}|')
+                                               f'\nОписание: |{value[5]}|\n')
             count += 1
 
     # ------------------------------------------------------------------------------------------------------------------
@@ -354,53 +364,120 @@ class BooksComics(QMainWindow):
         super().__init__()
         uic.loadUi('books_comics.ui', self)
 
-    # TODO: Сделать сортировку по Критериям пользователя
+        self.data_criteria_books = set()
 
-    # ----------------------------------------------Основные Критерии---------------------------------------------------
         # Кнопки Основных критерий
+
         self.btn_date_DESC_books.clicked.connect(self.output_of_books_by_date)
         self.btn_name_DESC_books.clicked.connect(self.output_of_books_by_name)
 
-        #
+        self.btn_exit.clicked.connect(self.exit)
 
-        # №. book_name, [release], author, (style), toms
-        # 4. Восхождение Героя Щита. Том 11, [2021], Кю Айя, (Манга), 12
+        # Критерии
+
+        self.checkBox.clicked.connect(self.serials_sort)
+        self.checkBox_2.clicked.connect(self.serials_sort)
+        self.checkBox_3.clicked.connect(self.serials_sort)
+        self.checkBox_4.clicked.connect(self.serials_sort)
+        self.checkBox_5.clicked.connect(self.serials_sort)
+
+        self.pushButton.clicked.connect(self.search_criteria)
+
+        # №. book_name, [release], author, (style), |toms|
+        # 4. Восхождение Героя Щита. Том 11, [2021], Кю Айя, (Манга), |12|
 
         count = 1
+        self.table_books.appendPlainText('№. book_name, [release], author, (style), |toms|\n')
         for value in sql.execute("SELECT * FROM data_books ORDER BY release DESC"):
             self.table_books.appendPlainText(f'{count}. '
                                              f'{value[0]}, '
                                              f'[{value[1]}], '
                                              f'{value[2]}, '
                                              f'({value[3]}), '
-                                             f'|{value[4]}|')
+                                             f'|{value[4]}|\n')
             count += 1
+    # ----------------------------------------------Основные Критерии---------------------------------------------------
 
     def output_of_books_by_date(self):
         self.table_books.clear()
+        self.table_books('№. book_name, [release], author, (style), |toms|\n')
         count = 1
-        for value in sql.execute("SELECT * FROM data_books ORDER BY release DESC"):
-            self.table_books.appendPlainText(f'{count}. '
-                                             f'{value[0]}, '
-                                             f'[{value[1]}], '
-                                             f'{value[2]}, '
-                                             f'({value[3]}), '
-                                             f'|{value[4]}|')
-            count += 1
+        if len(self.data_criteria_books) > 0:
+            for value in sql.execute(f"SELECT * FROM data_books WHERE style in {self.sort()} ORDER BY release DESC"):
+                self.table_books.appendPlainText(f'{count}. '
+                                                 f'{value[0]}, '
+                                                 f'[{value[1]}], '
+                                                 f'({value[2]}), '
+                                                 f'{value[3]}, '
+                                                 f'|{value[4]}|\n')
+                count += 1
+        else:
+            for value in sql.execute("SELECT * FROM data_books ORDER BY release DESC"):
+                self.table_books.appendPlainText(f'{count}. '
+                                                 f'{value[0]}, '
+                                                 f'[{value[1]}], '
+                                                 f'{value[2]}, '
+                                                 f'({value[3]}), '
+                                                 f'|{value[4]}|\n')
+                count += 1
 
     def output_of_books_by_name(self):
         self.table_books.clear()
+        self.table_books('№. book_name, [release], author, (style), |toms|\n')
         count = 1
-        for value in sql.execute("SELECT * FROM data_books ORDER BY book_name ASC"):
+        if len(self.data_criteria_books) > 0:
+            for value in sql.execute(f"SELECT * FROM data_books WHERE style in {self.sort()} ORDER BY book_name ASC"):
+                self.table_books.appendPlainText(f'{count}. '
+                                                 f'{value[0]}, '
+                                                 f'[{value[1]}], '
+                                                 f'({value[2]}), '
+                                                 f'{value[3]}, '
+                                                 f'|{value[4]}|\n')
+                count += 1
+        else:
+            for value in sql.execute("SELECT * FROM data_books ORDER BY book_name ASC"):
+                self.table_books.appendPlainText(f'{count}. '
+                                                 f'{value[0]}, '
+                                                 f'[{value[1]}], '
+                                                 f'{value[2]}, '
+                                                 f'({value[3]}), '
+                                                 f'|{value[4]}|\n')
+                count += 1
+
+    # --------------------------------------------Сортировка Критерий---------------------------------------------------
+
+        # Входные данные: {'Фантастика', 'Ужасы'}
+        # Возращает: ('Фантастика', 'Ужасы')
+        # Нужно для команды в базу данных и для избежание не верных выводов данных
+        # SELECT * FROM data WHERE style in ('Фантастика', 'Ужасы')
+
+    def sort(self):
+        return f'''('{"', '".join(self.data_criteria_books)}')'''
+
+    def serials_sort(self, state):
+        if state:
+            self.data_criteria_books.add(self.sender().text())
+        else:
+            self.data_criteria_books.remove(self.sender().text())
+
+    def search_criteria(self):
+        self.table_books.clear()
+        self.table_books.appendPlainText('№. book_name, [release], author, (style), |toms|\n')
+        count = 1
+        for value in sql.execute(f"SELECT * FROM data_books WHERE style in {self.sort()} ORDER BY release DESC"):
             self.table_books.appendPlainText(f'{count}. '
                                              f'{value[0]}, '
                                              f'[{value[1]}], '
-                                             f'{value[2]}, '
-                                             f'({value[3]}), '
-                                             f'|{value[4]}|')
+                                             f'({value[2]}), '
+                                             f'{value[3]}, '                                              
+                                             f'|{value[4]}|\n')
             count += 1
 
     # ------------------------------------------------------------------------------------------------------------------
+
+    def exit(self):
+        books_and_comics.close()
+        ex.show()
 
 
 if __name__ == '__main__':
